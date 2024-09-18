d7518a3fdaecdf5927e3c7b3af815f03cf4e1988
polinam <polinam@polina>
2024-09-19 00:40:31 +0300

    Add calculator.py file with main() function

---
+++ HW2_Malysheva/calculator.py
@@ -0,0 +1,23 @@
+def main(string):
+  for_calculating = string.split()
+  if '+' in for_calculating:
+    return summation()
+  elif '-' in for_calculating:
+    return substraction()
+  elif '*' in for_calculating:
+    return multiplication()
+  elif '/' in for_calculating:
+    return dividing()
+  else:
+    print('Что-то не так с введенными данными, проверь!')
+def summation():
+  pass
+def substraction():
+  pass
+def multiplication():
+  pass
+def dividing():
+  pass
+
+string = input('Введите математическую операцию: ')
+print(main(string))
