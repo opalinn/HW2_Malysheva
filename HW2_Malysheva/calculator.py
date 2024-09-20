dfb73761a9b790a619deb3962735449a42a91ee6
Anastasia Pomeranets <anapomerash@gmail.com>
2024-09-20 09:14:04 +0300

    Реализована функция деления с обработкой деления на ноль + дополнен файл README.md с описанием функции

--- HW2_Malysheva/calculator.py
+++ HW2_Malysheva/calculator.py
@@ -1,23 +1,37 @@
 def main(string):
-  for_calculating = string.split()
-  if '+' in for_calculating:
-    return summation()
-  elif '-' in for_calculating:
-    return substraction()
-  elif '*' in for_calculating:
-    return multiplication()
-  elif '/' in for_calculating:
-    return dividing()
-  else:
-    print('Что-то не так с введенными данными, проверь!')
-def summation():
-  pass
-def substraction():
-  pass
-def multiplication():
-  pass
-def dividing():
-  pass
+    for_calculating = string.split()
+    if "+" in for_calculating:
+        return summation()
+    elif "-" in for_calculating:
+        return substraction()
+    elif "*" in for_calculating:
+        return multiplication()
+    elif "/" in for_calculating:
+        return dividing()
+    else:
+        print("Что-то не так с введенными данными, проверь!")
 
-string = input('Введите математическую операцию: ')
+
+def summation():
+    pass
+
+
+def substraction():
+    pass
+
+
+def multiplication():
+    pass
+
+
+def dividing():
+    elements = string.split()
+    num1 = float(elements[0])
+    num2 = float(elements[2])
+    if num2 == 0:
+        return "Ошибка: деление на ноль."
+    return num1 / num2
+
+
+string = input("Введите математическую операцию: ")
 print(main(string))
